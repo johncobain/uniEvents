fe9e8d07e6ef10a892d72037e4e141339001588e
Andrey Gomes <andreygdsn@gmail.com>
2025-05-23 14:33:52 -0300

    feat: managers

--- src/main/java/br/edu/ifba/inf0008/uniEvents/services/EventManager.java
+++ src/main/java/br/edu/ifba/inf0008/uniEvents/services/EventManager.java
@@ -1,11 +1,50 @@
 package br.edu.ifba.inf0008.uniEvents.services;
 
+import java.util.ArrayList;
+
+import br.edu.ifba.inf0008.uniEvents.model.events.Event;
+import br.edu.ifba.inf0008.uniEvents.model.participants.Participant;
 import br.edu.ifba.inf0008.uniEvents.repository.EventRepository;
-import br.edu.ifba.inf0008.uniEvents.repository.ParticipantRepository;
 
 public class EventManager {
-  
-  public EventManager(EventRepository eventRepository, ParticipantRepository participantRepository) {
+  private final EventRepository eventRepository;
+
+  public static ArrayList<Event> events = new ArrayList<>();
+
+  public EventManager(EventRepository eventRepository) {
+    this.eventRepository = eventRepository;
+  }
+
+  public void addEvent(Event event){
+    events.add(event);
+    eventRepository.addEvent(event);
+  }
+
+  public void addParticipantToEvent(Event event, Participant participant){
+    event.addParticipant(participant);
+    // eventRepository.addParticipantToEvent(event, participant);
+  }
     
+
+  public void removeEvent(Event event){
+    events.remove(event);
+    eventRepository.removeEvent(event);
+  }
+
+  public void updateEvent(Event event){
+    eventRepository.updateEvent(event, event.getCode());
+  }
+
+  public Event getEvent(String code){
+    for (Event event : events) {
+      if (event.getCode().equals(code)) {
+        return event;
+      }
+    }
+    return null;
+  }
+
+  public ArrayList<Event> getAllEvents() {
+    return events;
   }
 }
--- src/main/java/br/edu/ifba/inf0008/uniEvents/services/ParticipantManager.java
+++ src/main/java/br/edu/ifba/inf0008/uniEvents/services/ParticipantManager.java
@@ -1,10 +1,68 @@
 package br.edu.ifba.inf0008.uniEvents.services;
 
+import java.util.ArrayList;
+
+import br.edu.ifba.inf0008.uniEvents.model.events.Event;
+import br.edu.ifba.inf0008.uniEvents.model.participants.Participant;
 import br.edu.ifba.inf0008.uniEvents.repository.ParticipantRepository;
 
 public class ParticipantManager {
+  private final ParticipantRepository participantRepository;
+
+  public static ArrayList<Participant> participants = new ArrayList<>();
   
   public ParticipantManager(ParticipantRepository participantRepository) {
-    
+    this.participantRepository = participantRepository;
   }
+
+  public void addParticipant(Participant participant){
+    participants.add(participant);
+    participantRepository.addParticipant(participant);
+  }
+
+  public void removeParticipant(Participant participant){
+    participants.remove(participant);
+    participantRepository.removeParticipant(participant);
+  }
+
+  public void updateParticipant(Participant participant){
+    participantRepository.updateParticipant(participant, participant.getCpf());
+  }
+
+  public Participant getParticipant(String cpf){
+    for (Participant participant : participants) {
+      if (participant.getCpf().equals(cpf)) {
+        return participant;
+      }
+    }
+    return null;
+  }
+
+  public ArrayList<Event> getEventsByParticipant(Participant participant){
+    ArrayList<Event> events = new ArrayList<>();
+    for (Event event : EventManager.events) {
+      if (event.getParticipants().contains(participant)) {
+        events.add(event);
+      }
+    }
+    return events;
+  }
+
+  public void clearEventsByParticipant(Participant participant){
+    for (Event event : EventManager.events) {
+      if (event.getParticipants().contains(participant)) {
+        event.removeParticipant(participant);
+      }
+    }
+  }
+
+  public void clearAllParticipants() {
+    participants.clear();
+    participantRepository.clearAllParticipants();
+  }
+
+  public ArrayList<Participant> getAllParticipants() {
+    return participants;
+  }
+
 }
