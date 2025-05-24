4e1278dded6b75f5e6b7813a8ec8a6319739981e
Andrey <andreygdsn@gmail.com>
2025-05-24 16:54:01 -0300

    feat: add integer validation

--- src/main/java/br/edu/ifba/inf0008/uniEvents/utils/Validation.java
+++ src/main/java/br/edu/ifba/inf0008/uniEvents/utils/Validation.java
@@ -1,5 +1,23 @@
 package br.edu.ifba.inf0008.uniEvents.utils;
 
 public class Validation {
-  
+  public static void validateCpf(String cpf){}
+
+  public static void validateEmail(String email){}
+
+  public static void validatePhone(String phone){}
+
+  public static void validateDate(String date){}
+
+  public static int isInteger(String value){
+    if (value == null || value.isEmpty() || value.isBlank()) {
+      throw new IllegalArgumentException("Input cannot be empty!");
+    }
+    try {
+      int parsedValue = Integer.parseInt(value);
+      return parsedValue;
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("Invalid input, not an integer!");
+    }
+  }
 }
