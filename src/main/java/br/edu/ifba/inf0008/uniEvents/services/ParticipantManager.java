dcd0a27b93704f011c14e8d2412a399e81543e56
Andrey Gomes <andreygdsn@gmail.com>
2025-05-28 09:50:11 -0300

    refactor: participantManager to use interface

--- src/main/java/br/edu/ifba/inf0008/uniEvents/services/ParticipantManager.java
+++ src/main/java/br/edu/ifba/inf0008/uniEvents/services/ParticipantManager.java
@@ -13,7 +13,7 @@ import br.edu.ifba.inf0008.uniEvents.model.participants.enums.AcademicTitle;
 import br.edu.ifba.inf0008.uniEvents.model.participants.enums.Course;
 import br.edu.ifba.inf0008.uniEvents.repository.ParticipantRepository;
 
-public class ParticipantManager {
+public class ParticipantManager implements IManager<Participant> {
   private final ParticipantRepository participantRepository;
 
   public static LinkedHashMap<String, Participant> participants;
@@ -23,22 +23,25 @@ public class ParticipantManager {
     participants = participantRepository.getAll();
   }
 
+  @Override
   public void add(Participant participant){
     participants.put(participant.getCpf(), participant);
     participantRepository.add(participant);
   }
 
-  public void remove(String cpf, EventManager eventManager) {
-    clearParticipantsInEvents(cpf, eventManager);
+  @Override
+  public void remove(String cpf) {
     participants.remove(cpf);
     participantRepository.remove(cpf);
   }
 
+  @Override
   public void update(String cpf, Participant updatedParticipant) {
     participants.put(cpf, updatedParticipant);
     participantRepository.update(updatedParticipant, cpf);
   }
 
+  @Override
   public Participant get(String cpf){
     if(participants.get(cpf) == null) {
       return null;
@@ -46,20 +49,29 @@ public class ParticipantManager {
     return participants.get(cpf);
   }
 
-  public void clearParticipantsInEvents(String cpf, EventManager eventManager) {
+  @Override
+  public void clear() {
+    participants.clear();
+    participantRepository.clear();
+  }
+
+  @Override
+  public LinkedHashMap<String, Participant> getAll() {
+    return participants;
+  }
+
+  public void clearParticipantsInEvents(EventManager eventManager) {
+    for (String cpf : participants.keySet()) {
+      clearParticipantInEvents(cpf, eventManager);
+    }
+  }
+
+  public void clearParticipantInEvents(String cpf, EventManager eventManager) {
     for (Event event : EventManager.events.values()) {
       if (event.getParticipants().containsKey(cpf)) {
         eventManager.removeParticipant(event.getCode(), cpf);
       }
     }
-  }
-
-  public void clear(EventManager eventManager) {
-    for (String cpf : participants.keySet()) {
-      clearParticipantsInEvents(cpf, eventManager);
-    }
-    participants.clear();
-    participantRepository.clear();
   }
 
   public Boolean isCpfAlreadyInUse(String cpf) {
@@ -73,10 +85,6 @@ public class ParticipantManager {
       }
     }
     return false;
-  }
-
-  public LinkedHashMap<String, Participant> getAll() {
-    return participants;
   }
 
   public void createStudent(String name, String cpf, String email, String phone, LocalDate birthDate, String studentId, Course course, int currentSemester, AcademicStatus academicStatus, double gpa, String campus, LocalDate enrollmentDate) {
